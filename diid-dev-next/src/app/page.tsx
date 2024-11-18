31ee928f0d216431570c21f347576d41482f03ba
RenFrost <diidnugroho@gmail.com>
2024-11-18 21:45:24 +0700

    chore: moving portfolio card from homepage to portfolio page

--- diid-dev-next/src/app/page.tsx
+++ diid-dev-next/src/app/page.tsx
@@ -1,10 +1,9 @@
-import PortfolioCard from "@/components/PortfolioCard";
 
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 gap-8">
-      <PortfolioCard />
+    <div>
+      <h1>This is Homepage</h1>
     </div>
   );
 }
--- diid-dev-next/src/app/portfolio/page.tsx
+++ diid-dev-next/src/app/portfolio/page.tsx
@@ -1,9 +1,10 @@
+import PortfolioCard from '@/components/PortfolioCard'
 import React from 'react'
 
 export default function Portfolio () {
   return (
-    <div>
-        <h1>This is Portfolio Page</h1>
+    <div className="grid grid-cols-3 gap-8">
+      <PortfolioCard />
     </div>
   )
 }
