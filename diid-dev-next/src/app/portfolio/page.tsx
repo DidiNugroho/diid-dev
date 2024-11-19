b10f74f17377f4407e154ac8b985ad58618d8f5b
RenFrost <diidnugroho@gmail.com>
2024-11-19 21:37:53 +0700

    chore: modifying portfolio card with real project

--- diid-dev-next/src/app/portfolio/page.tsx
+++ diid-dev-next/src/app/portfolio/page.tsx
@@ -2,9 +2,9 @@ import PortfolioCard from '@/components/PortfolioCard'
 import React from 'react'
 import projectsData, { Project } from '../../../data/projectData'
 
-export default function Portfolio () {
+export default function Portfolio() {
   return (
-    <div className="grid grid-cols-3 gap-8">
+    <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {projectsData.map((project: Project) => (
         <div key={project.id}>
           <PortfolioCard {...project} />
--- diid-dev-next/src/components/PortfolioCard.tsx
+++ diid-dev-next/src/components/PortfolioCard.tsx
@@ -6,13 +6,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Github, Link } from "lucide-react";
 
 import React from "react";
 import { Project } from "../../data/projectData";
+import Image from "next/image";
 
 export default function PortfolioCard(project: Project) {
   return (
-    <div>
+    <div className="p-8">
       <Card>
         <CardHeader>
           <CardTitle>{project.title}</CardTitle>
@@ -21,9 +23,27 @@ export default function PortfolioCard(project: Project) {
           <CardDescription>
             <p>{project.description}</p>
           </CardDescription>
+          <div className="mt-4">
+            <Image src={project.imageUrl} width={500} height={500} alt="Project Image" />
+          </div>
         </CardContent>
-        <CardFooter>
-          <a href="/portfolio">View Portfolio</a>
+        <CardFooter className="flex space-x-4">
+          <a
+            href={project.githubUrl} 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-black"
+          >
+            <Github className="h-8 w-8" />
+          </a>
+          <a
+            href={project.liveUrl} 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-black"
+          >
+            <Link className="h-8 w-8" />
+          </a>
         </CardFooter>
       </Card>
     </div>
