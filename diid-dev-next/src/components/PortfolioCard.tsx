196816460b025521e48a50b09f913fa32de109ef
RenFrost <diidnugroho@gmail.com>
2024-11-20 19:12:15 +0700

    chore: modifying portfolio card

--- diid-dev-next/src/components/PortfolioCard.tsx
+++ diid-dev-next/src/components/PortfolioCard.tsx
@@ -7,6 +7,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Github, Link } from "lucide-react";
+import { Button } from "@/components/ui/button"
 
 import React from "react";
 import { Project } from "../../data/projectData";
@@ -26,6 +27,12 @@ export default function PortfolioCard(project: Project) {
           <div className="mt-4">
             <Image src={project.imageUrl} width={500} height={500} alt="Project Image" />
           </div>
+          <div className="mt-4">
+            <p>Tech Stack:</p>
+            {project.technologies.map((tech, index) => (
+              <Button key={index} variant="secondary" className="mr-2 mb-2 mt-2">{tech}</Button>
+            ))}
+          </div>
         </CardContent>
         <CardFooter className="flex space-x-4">
           <a
@@ -34,7 +41,7 @@ export default function PortfolioCard(project: Project) {
             rel="noopener noreferrer"
             className="hover:text-black"
           >
-            <Github className="h-8 w-8" />
+            <Github className="h-6 w-6" />
           </a>
           <a
             href={project.liveUrl} 
@@ -42,7 +49,7 @@ export default function PortfolioCard(project: Project) {
             rel="noopener noreferrer"
             className="hover:text-black"
           >
-            <Link className="h-8 w-8" />
+            <Link className="h-6 w-6" />
           </a>
         </CardFooter>
       </Card>
